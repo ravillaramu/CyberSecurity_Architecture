1f1eeacfe880bdfa67a7d2fdc6f396ac0a37f54b
AtchayaThangavelu <110536458+AtchayaThangavelu@users.noreply.github.com>
2023-01-09 15:57:22 +0530

    Add files via upload

---
+++ SecretManager/var.tf
@@ -0,0 +1,17 @@
+variable "length" {
+  default = "8"
+}
+variable "special" {
+  default = "true"
+}
+variable "special" {
+  default = "true"
+}
+variable "secretname" {
+  
+}
+variable "username" {
+  default = "Admin"
+}
+
+
